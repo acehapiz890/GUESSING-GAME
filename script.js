1ba36ad5856c05606ddc6a9a3fe507bc924d3296
acehapiz890 <ashapiz@sscgi.com>
2025-07-03 16:27:41 +0800

    2nd commit final version of the game with all features

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = "ACES HAPIZ WINS";
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 123;
-console.log(document.querySelector('.guess').value);
-*/
-
-/*
-add event listener example template
-document.querySelector(' .check').addEventListener('click', function () {}
-*/
 
 // generates the random number
-const secretNum = Math.trunc(Math.random() * 20) + 1;
+let secretNum = Math.trunc(Math.random() * 20) + 1;
 
 // display the random number in the box
 document.querySelector('.number').textContent = `?`;
 
 //variable used for score counting 
 let score = 20;
+
+//variable used to get high score
+let highScore = 0;
 
 // FUNCTIONS //
 function checkScoreHigh() {
@@ -52,16 +41,13 @@ function checkScoreLow() {
     }
 }
 
-document.querySelector('.again').addEventListener('click', function () {
-    document.querySelector('.message').textContent = `Start guessing...`;
-    document.querySelector('.number').textContent = `?`;
-    document.querySelector('.label-score').textContent = `Score: ${score =20}`;
-    document.querySelector('.guess').value = ``;
-    document.querySelector('.number').textContent = secretNum;
-    document.querySelector('body').style.backgroundColor = `#222`;
-    document.querySelector('.number').style.width = `15rem`;
-    console.log('this is working');
-});
+//fuction to check highscore
+function checkHighScore() {
+    if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = `${highScore}`;
+    }
+}
 
 // event listener / conditions of the game
 document.querySelector('.check').addEventListener('click', function () {
@@ -75,9 +61,21 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('body').style.backgroundColor = ' #60b347';
         document.querySelector('.number').style.width = `30rem`;
         document.querySelector('.number').textContent = secretNum;
+        checkHighScore();
     } else if (guess < secretNum) {
         checkScoreLow();
     } else {
         checkScoreHigh();
     }
+});
+
+// event listener to reset the game
+document.querySelector('.again').addEventListener('click', function () {
+    document.querySelector('.message').textContent = `Start guessing...`;
+    document.querySelector('.label-score').textContent = `Score: ${score =20}`;
+    document.querySelector('.guess').value = ``;
+    document.querySelector('body').style.backgroundColor = `#222`;
+    document.querySelector('.number').style.width = `15rem`;
+    document.querySelector('.number').textContent = `?`;
+    secretNum = Math.trunc(Math.random() * 20) + 1;
 });
